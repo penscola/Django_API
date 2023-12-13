68552f57a27d92d3b44f62d86a2287364db1a54a
penscola <penscolashackletonfelix@gmail.com>
2023-12-13 12:57:07 +0300

    added my apps in settings & path

--- django_api/urls.py
+++ django_api/urls.py
@@ -6,7 +6,7 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views. Home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from django_api import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('drinks', views.drink_list),
+    path('drinks/<int:id>', views.drink_details)
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
